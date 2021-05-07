6806c12c7d279d09bbd24036b8da638f28011578
Tingting Thompson <ttthompson@email.arizona.edu>
2021-05-06 23:55:49 -0500

    Upload

--- ToCSV.py
+++ ToCSV.py
@@ -1,8 +1,10 @@
 import pandas as pd, yfinance as yf, numpy as np, json
 import os, sys
 
-ticker = "GME"
-sdate, edate = "2021-01-25", "2021-02-02"
-df = yf.download(ticker, start = sdate, end = edate, interval = "1h")
+sdate, edate = "2021-01-25", "2021-02-03"
+df = yf.download("GME", start = sdate, end = edate, interval = "1h")
 path = os.getcwd()
 df.to_csv(path+"/GME.csv")
+df = yf.download("AMC", start = sdate, end = edate, interval = "1h")
+path = os.getcwd()
+df.to_csv(path+"/AMC.csv")
